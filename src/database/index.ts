eacc76bc72012d6e7636072daf07f9a6b495545b
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-08-04 21:46:46 -0300

    Configuração conexão com BD com TypeORM

---
+++ src/database/index.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+
+// entities
+import { Auth } from './../models/Auth'
+
+export default async () => {
+  await createConnection({
+    name: 'default',
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'proffy_db',
+    synchronize: true,
+    entities: [
+      Auth
+    ]
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import app from './app'
+import database from './database'
+
+database()
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running!')

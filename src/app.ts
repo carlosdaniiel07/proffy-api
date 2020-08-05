3e11bddace0185c9d6be6fdba33dc5a6d905fecf
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-08-04 22:59:16 -0300

    Middleware para tratamento global de erros

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import appRoutes from './routes'
 
+import globalErrorHandler from './middlewares/globalErrorHandler'
+
 const app = express()
 const router = express.Router()
 
@@ -8,5 +10,6 @@ appRoutes.forEach(route => route(router))
 
 app.use(express.json())
 app.use(router)
+app.use(globalErrorHandler())
 
 export default app
